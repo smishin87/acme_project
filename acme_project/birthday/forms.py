79558a45ec7e98884f40eb359f017c3797c26375
Sergei Mishin <SMishinMail@gmail.com>
2023-06-07 22:15:43 +0700

    Replaces Form with ModelForm Adds Utils Uses django_bootstrap5 styles

--- acme_project/birthday/forms.py
+++ acme_project/birthday/forms.py
@@ -1,12 +1,13 @@
 from django import forms
 
+from .models import Birthday
 
-class BirthdayForm(forms.Form):
-    first_name = forms.CharField(label='Имя', max_length=20)
-    last_name = forms.CharField(
-        label='Фамилия', required=False, help_text='Необязательное поле'
-    )
-    birthday = forms.DateField(
-        label='Дата рождения',
-        widget=forms.DateInput(attrs={'type': 'date'})
-    )
+
+class BirthdayForm(forms.ModelForm):
+
+    class Meta:
+        fields = '__all__'
+        model = Birthday
+        widgets = {
+            'birthday': forms.DateInput({'type': 'date'}),
+        }
